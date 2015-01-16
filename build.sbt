b27e12094d9d460709aadf6b62eac3f4e163ca39
Cihan Emre <cihan@vngrs.com>
2015-01-16 10:15:59 +0200

    use typesafe config

--- build.sbt
+++ build.sbt
@@ -2,5 +2,7 @@ libraryDependencies ++= Seq(
 	"org.apache.kafka" % "kafka_2.10" % "0.8.1"
     	exclude("javax.jms", "jms")
     	exclude("com.sun.jdmk", "jmxtools")
-    	exclude("com.sun.jmx", "jmxri")
+    	exclude("com.sun.jmx", "jmxri"),
+	"com.typesafe" % "config" % "1.2.1",
+	"org.scalatest" % "scalatest_2.10" % "2.2.1" % "test"
 )
---
+++ src/main/scala/KafkaConfig.scala
@@ -0,0 +1,35 @@
+import java.util.Properties
+
+import com.typesafe.config.ConfigFactory
+import KafkaConfig._
+
+trait KafkaConfig extends Properties {
+
+  private val consumerPrefixWithDot = consumerPrefix + "."
+  private val producerPrefixWithDot = producerPrefix + "."
+
+  private val allKeys = Seq(groupId, zookeeperConnect, brokers, serializer, partitioner, requiredAcks)
+  private lazy val typesafeConfig = ConfigFactory.load()
+  allKeys.map { key =>
+    if (typesafeConfig.hasPath(key))
+      put(key.replace(consumerPrefixWithDot, "").replace(producerPrefixWithDot, ""), typesafeConfig.getString(key))
+  }
+}
+
+object KafkaConfig {
+
+  val consumerPrefix = "consumer"
+  val producerPrefix = "producer"
+
+  //Consumer keys
+  val groupId = s"$consumerPrefix.group.id"
+  val zookeeperConnect = s"$consumerPrefix.zookeeper.connect"
+
+  //Producer keys
+  val brokers = s"$producerPrefix.metadata.broker.list"
+  val serializer = s"$producerPrefix.serializer.class"
+  val partitioner = s"$producerPrefix.partitioner.class"
+  val requiredAcks = s"$producerPrefix.request.required.acks"
+
+  def apply() = new KafkaConfig {}
+}
