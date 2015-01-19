6e283d1074d6887f468fda1c8d35f7c6c5dfe9e9
Cihan Emre <cihan@vngrs.com>
2015-01-19 15:33:56 +0200

    add complex type producer example

--- src/main/scala/example/producer/Producer.scala
+++ src/main/scala/example/producer/Producer.scala
@@ -1,11 +1,13 @@
 package example.producer
 
+import java.util.Properties
+
 import example.utils.KafkaConfig
 import kafka.producer.{KeyedMessage, ProducerConfig, Producer => KafkaProducer}
 
 case class Producer[A](topic: String) {
-  private val config = new ProducerConfig(KafkaConfig())
-  private val producer = new KafkaProducer[A, A](config)
+  protected val config = new ProducerConfig(KafkaConfig())
+  private lazy val producer = new KafkaProducer[A, A](config)
 
   def send(message: A) = sendMessage(producer, keyedMessage(topic, message))
 
@@ -19,4 +21,11 @@ case class Producer[A](topic: String) {
 
   private def keyedMessage(topic: String, message: A): KeyedMessage[A, A] = new KeyedMessage[A, A](topic, message)
   private def sendMessage(producer: KafkaProducer[A, A], message: KeyedMessage[A, A]) = producer.send(message)
+}
+
+
+object Producer {
+  def apply[T](topic: String, props: Properties) = new Producer[T](topic) {
+    override val config = new ProducerConfig(props)
+  }
 }
